dfbf22e65ea4811d6d62fac20c3886a95c4cfe6c
Leanca Alin <valentin.leanca@randstaddigital.com>
2024-02-21 09:54:42 +0200

    limitareaobiectelor actualizata

--- Autoturism.cpp
+++ Autoturism.cpp
@@ -1,15 +1,13 @@
 #include "Autoturism.h"
 #include <iostream>
 
-int Autoturism::numarObiecte = 0;
-
 Autoturism::Autoturism(const std::string combustibil, const std::string caroserie,const std::string tractiune, int putere,const std::string echipare, int* vin)
 	: Automobil( combustibil, caroserie, tractiune, putere, echipare, vin) {
-		if (numarObiecte >= 1){
-			std::cout << "Nu se mai poate crea un alt obiect" << std::endl;
-		}else{
+		//verifica daca numarul maxim de obiecte a fost atins
+		if (numarObiecte < numarMaximObiecte){
 			numarObiecte++;
-			std::cout << "Apelare Constructor Autoturism" << std::endl;
+		}else{
+			std::cout << "Nu se mai poate crea un alt obiect numarul maxim a fost atins." << std::endl;
 		}
 }
 
@@ -23,6 +21,15 @@ void Autoturism::afiseazaInfo() const {
 	std::cout << "Echipare: " << Echipare << std::endl;
 	std::cout << "VIN: " << *Vin << std::endl;
 }
+
+//functia pentru setarea numarului maxim de obiecte
+void Autoturism::seteazaNumarMaximObiecte(int limita){
+	numarMaximObiecte = limita;
+}
+
+//initializare variabile
+int Autoturism::numarMaximObiecte = 1;
+int Autoturism::numarObiecte = 0;
 
 Autoturism::~Autoturism() {
 	std::cout << "Apelare Destructor Autoturism" << std::endl;
--- main.cpp
+++ main.cpp
@@ -4,20 +4,29 @@
 #include "Automobil.h"
 
 int main() {
+	Autoturism::seteazaNumarMaximObiecte(1);
+
 	int* vinAutoturism = new int(123456); //VIN pentru masina autoturism
 	Automobil* autoturism = new Autoturism("Benzina", "Berlina", "Fata", 130, "Standard", vinAutoturism);
 
-	//int* vinAutoturism1 = new int(234567); //VIN pentru masina autoturism
-	//Automobil* autoturism1= new Autoturism("Benzina", "Break", "Spate", 120, "Full", vinAutoturism);
+	int* vinAutoturism1 = new int(234567); //VIN pentru masina autoturism
+	Automobil* autoturism1= new Autoturism("Benzina", "Break", "Spate", 120, "Full", vinAutoturism1);
+
+	//int* vinAutoturism2 = new int(234567); //VIN pentru masina autoturism
+	//Automobil* autoturism2= new Autoturism("Motorina", "Cabrio", "Spate", 190, "Full", vinAutoturism2);
+
+	autoturism->afiseazaInfo();
+	std::cout << "\n";
+	autoturism1->afiseazaInfo();
+	std::cout << "\n";
+	//autoturism2->afiseazaInfo();
+	//std::cout << "\n";
 	
 	int* vinAutoutilitara = new int(654321); //VIN pentru masina autoutilitara
 	Automobil* autoutilitara = new Autoutilitara("Motorina", "Duba", "Spate", 150, "Full", vinAutoutilitara);
 
-	autoturism->afiseazaInfo();
-	std::cout << "\n";
-	//autoturism1->afiseazaInfo();
-	//std::cout << "\n";
 	autoutilitara->afiseazaInfo();
+	std::cout << "\n";
 	
 	delete autoturism;
 	delete autoutilitara;
